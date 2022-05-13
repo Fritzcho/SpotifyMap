5201213280575b85967f0f4e49236fdc865af823
Fritzcho <alireza.ramezani@weknowit.se>
2022-05-13 14:14:37 +0200

    Removed unnecessary stuff

--- spotifymap/src/screens/HomeScreen/App.jsx
+++ spotifymap/src/screens/HomeScreen/App.jsx
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import {CLIENT_ID, CLIENT_SECRET, REDIRECT_URI, RESPONSE_TYPE, AUTH_ENDPOINT} from '../../utils/spotifyclient'
 
@@ -6,18 +5,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <a href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}>Login to Spotify</a>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+        <a href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}>
+          Login to Spotify
         </a>
       </header>
     </div>
