45e0b58991c1f3959f4e3dcbb65e60f7c753f549
Fritzcho <alireza.ramezani@weknowit.se>
2022-05-13 14:12:19 +0200

    Started working on integrating spotify api

--- spotifymap/src/screens/HomeScreen/App.jsx
+++ spotifymap/src/screens/HomeScreen/App.jsx
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import {CLIENT_ID, CLIENT_SECRET, REDIRECT_URI, RESPONSE_TYPE, AUTH_ENDPOINT} from '../../utils/spotifyclient'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <a href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}>Login to Spotify</a>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
---
+++ spotifymap/src/utils/spotifyclient.jsx
@@ -0,0 +1,5 @@
+export const CLIENT_ID = 'ba8c147b930b42fe9e01a1a1266299d4'; // Your client id
+export const CLIENT_SECRET = 'd7b6ef9e6d4141f48343821672e50ac1'; // Your secret
+export const REDIRECT_URI = "http://localhost:3000";
+export const AUTH_ENDPOINT = 'https://accounts.spotify.com/authorize';
+export const RESPONSE_TYPE = 'token';
