269216046a28ffaada802d3b89caab2c690f4f3d
Fritzcho <alireza.ramezani@weknowit.se>
2022-05-13 14:24:41 +0200

    Readded index.html after a fuckup

--- spotifymap/src/screens/HomeScreen/App.jsx
+++ spotifymap/src/screens/HomeScreen/App.jsx
@@ -1,7 +1,25 @@
+import React, {useEffect, useState} from 'react';
 import './App.css';
 import {CLIENT_ID, CLIENT_SECRET, REDIRECT_URI, RESPONSE_TYPE, AUTH_ENDPOINT} from '../../utils/spotifyclient'
 
 function App() {
+  const [token, setToken] = useState("")
+  
+  useEffect(() => {
+    const hash = window.location.hash
+    let token = window.localStorage.getItem("token")
+
+    if (!token && hash) {
+        token = hash.substring(1).split("&").find(elem => elem.startsWith("access_token")).split("=")[1]
+
+        window.location.hash = ""
+        window.localStorage.setItem("token", token)
+    }
+
+    setToken(token)
+
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
