80678297b8768819a86acd3cf3fdbe0b2628edfb
Fritzcho <alireza.ramezani@weknowit.se>
2022-05-13 15:14:06 +0200

    Fixed spotify authentiaction

--- spotifymap/src/screens/HomeScreen/App.jsx
+++ spotifymap/src/screens/HomeScreen/App.jsx
@@ -1,23 +1,18 @@
 import React, {useEffect, useState} from 'react';
 import './App.css';
-import {CLIENT_ID, CLIENT_SECRET, REDIRECT_URI, RESPONSE_TYPE, AUTH_ENDPOINT} from '../../utils/spotifyclient'
+import {CLIENT_ID, REDIRECT_URI, RESPONSE_TYPE, AUTH_ENDPOINT} from '../../utils/spotifyclient'
 
-function App() {
-  const [token, setToken] = useState("")
-  
+function App() {  
   useEffect(() => {
     const hash = window.location.hash
-    let token = window.localStorage.getItem("token")
+    let token = window.localStorage.getItem("spotifytoken")
 
     if (!token && hash) {
         token = hash.substring(1).split("&").find(elem => elem.startsWith("access_token")).split("=")[1]
 
         window.location.hash = ""
-        window.localStorage.setItem("token", token)
+        window.localStorage.setItem("spotifytoken", token)
     }
-
-    setToken(token)
-
   }, [])
 
   return (
@@ -25,7 +20,7 @@ function App() {
       <header className="App-header">
         <a href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}>
           Login to Spotify
-        </a>
+        </a> 
       </header>
     </div>
   );
