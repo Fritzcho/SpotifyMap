fd12bcf884e1d9273fece7e33b0aaaa6f60441e5
Fritzcho <alireza.ramezani@weknowit.se>
2022-05-15 23:03:40 +0200

    Minor fixes

--- spotifymap/src/screens/HomeScreen/App.jsx
+++ spotifymap/src/screens/HomeScreen/App.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect} from 'react';
 import './App.css';
 import {CLIENT_ID, REDIRECT_URI, RESPONSE_TYPE, AUTH_ENDPOINT} from '../../utils/spotifyclient'
 
