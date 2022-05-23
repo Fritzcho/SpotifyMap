45e8e30fa0691a820dd446ae1aecff6f6ceca62e
SamuelJSandlund <ens18ssd@cs.umu.se>
2022-05-23 15:13:23 +0200

    let me pull

---
+++ spotifymap/src/components/Song/Song.jsx
@@ -0,0 +1,14 @@
+/*import React, {useState} from "react";
+
+function Song(){
+    return{
+        <div className="Song">
+            <div className="coverArt">
+
+            </div>
+            <div className="songInfo">
+
+            </div>
+        </div>
+    }
+}*/
